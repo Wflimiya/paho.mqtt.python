d9be270116185966840f5c14705a2f47ac3c7380
Wflimiya <19aj146@ms.dendai.ac.jp>
2022-10-03 15:07:17 +0900

    Create subscribe.py

---
+++ examples/subscribe.py
@@ -0,0 +1,10 @@
+import context 
+import paho.mqtt.subscribe as subscribe
+
+m = subscribe.simple(topics=['#'], hostname="eu1.cloud.thethings.network:8883", 
+                     port=8883, auth={'username':"19aj146@ttn",
+                                      'password':"NNSXS.733YNHPYC4QMWMX6QZNGPETXW7ANSGVFKSLVZGY.Y6ZVI52YCZJF4RDHKRWYX5NR3TV3KU5BSGFTBEWFPKSTYJ4BNMLQ"},
+                     msg_count=2)
+for a in m:
+    print(a.topic)
+    print(a.payload)
